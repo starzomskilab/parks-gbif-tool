d94d337593cec113e96c3836759f3b7a91d9f598
Julie Galloway <jgalloway@uvic.ca>
2019-06-02 11:26:04 -0700

    remove line to search for parks data

--- 01_load.R
+++ 01_load.R
@@ -21,8 +21,7 @@ Sys.setenv(GBIF_EMAIL = "jgalloway@uvic.ca")
 # pass in geojson file
 # install.packages("remotes")
 # remotes::install_github("bcgov/bcdata")
-library(bcdata)
-bcdc_search("parks")
 
+# Download BC parks data as sfc
 parks <- bcdc_get_geodata("1130248f-f1a3-4956-8b2e-38d29d3e4af7", crs = 4326)
 class(parks)
