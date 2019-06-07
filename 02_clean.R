5cfb6903e491ccecb25f2fcaecea0e9f331dfd20
Julie Galloway <jgalloway@uvic.ca>
2019-06-07 13:25:12 -0700

    add func to create wkts from list of polys

--- 02_clean.R
+++ 02_clean.R
@@ -4,15 +4,31 @@
 # extracting only provincial parks
 prov_parks <- filter(parks, PROTECTED_LANDS_DESIGNATION == "PROVINCIAL PARK")
 
-# add geometry column
-prov_parks <- st_geometry(prov_parks)
-
 # simplify parks geometry
 parks_simp <- ms_simplify(prov_parks, keep = 0.25)
 plot(parks_simp)
 
+# create list of parks
+park_names <- unique(prov_parks$PROTECTED_LANDS_NAME)
+
+# create list to store wkts
+park_wkts <- vector(length = length(park_names), mode = "list")
+names(park_wkts) <- park_names
+
+# func
+make_park_wkt <- function(data) {
+  # create wkt for single park
+  parkwkt <- st_as_text(data)
+}
+
+# map call
+park_wkts <- map(park_names, ~ {
+  data <- filter(prov_parks, protected_lands_name == .x)
+  parkwkt(data, .x)
+})
 # convert to text
 parks_wkt <- st_as_text(parks_geom)
+
 # saveRDS(parks_wkt, file = "parks-wkt")
 # parks_wkt <- readRDS("parks_wkt") # read saved from disk
 
