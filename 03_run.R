345ef14fcf826356d43add33baf67e884bf807b2
Julie Galloway <jgalloway@uvic.ca>
2019-06-07 20:22:18 -0700

    remove geometry param (duplicate)

--- 03_run.R
+++ 03_run.R
@@ -5,11 +5,11 @@ for (i in seq_along(park_wkts)) {
   queries[[i]] <- occ_download_prep(
     "hasCoordinate = true",
     "hasGeospatialIssue = false",
-    "geometry = parks_wkts",
     paste0("geometry = ", park_wkts[i])
   )
 }
 
+queries[1]
 # launch download cue
 out <- occ_download_queue(.list = queries)
 out
