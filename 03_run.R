cba1ba00586fcae2c3464d520644b68aeeb7ac40
Julie Galloway <jgalloway@uvic.ca>
2019-06-09 12:56:33 -0700

    remove discovery island search

--- 03_run.R
+++ 03_run.R
@@ -14,15 +14,6 @@ for (i in seq_along(park_wkts)) {
 out <- occ_download_queue(.list = queries, status_ping = 10)
 out
 
-# extract from parks wkt
-discisland <- park_wkts[[1]]
-class(discisland)
-library(wellknown)
-discisland <- polygon(discisland)
-
-# download for discovery island
-occ_download("geometry = discisland", "hasCoordinate = true")
-
 # full wkt download
 full_download <- occ_download_queue(
   occ_download("geometry = parks_wkt_full", "hasGeometry = true")
