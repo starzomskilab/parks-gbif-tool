8cd69724a53c9a5cdd2ac51239dbbda32aab009d
Julie Galloway <jgalloway@uvic.ca>
2019-06-09 22:03:33 -0700

    remove old wkts

--- scratch.R
+++ scratch.R
@@ -4,27 +4,24 @@
 skaha <- filter(parks_simp, protected_lands_name == "SKAHA BLUFFS PARK")
 skaha <- st_geometry(skaha)# add geometry column
 skaha <- st_cast(skaha, "POLYGON")
+skaha <- ms_simplify(skaha, keep = 0.05)
 skaha_wkt <- st_as_text(skaha)
+skaha_wkt
 skaha_wkt_simp <- geojson2wkt(unclass(wkt2geojson(skaha_wkt, feature = FALSE)), fmt = 5)
 skaha_wkt_simp
 
 # search for data within the parks boundary
 skaha_search <- occ_search(geometry = skaha_wkt_simp, hasCoordinate = T)
+skaha_search
 parks_search <- occ_search(geometry = parks_wkt_full, hasCoordinate = T, 
                            limit = 200)
-skaha_wkt_simp
-# download
-skaha_dl <- occ_download("geometry = POLYGON ((-119.56810 49.42843, -119.56800 49.43797, -119.56820 49.44625, 
-                         -119.55760 49.44621, -119.55730 49.46060, -119.54890 49.46054, -119.54970 49.45899, 
-                         -119.55010 49.45717, -119.54990 49.45651, -119.55040 49.45577, -119.55020 49.45465, 
-                         -119.55090 49.45287, -119.55200 49.45152, -119.55320 49.44921, -119.55450 49.44615, 
-                         -119.55470 49.44305, -119.55540 49.44275, -119.55580 49.44161, -119.55750 49.43965, 
-                         -119.55810 49.43738, -119.55860 49.43539, -119.55830 49.43446, -119.55130 49.43453, 
-                         -119.55130 49.43815, -119.54580 49.43820, -119.54590 49.44185, -119.52370 49.44206, 
-                         -119.52350 49.43482, -119.54020 49.43467, -119.54000 49.42743, -119.56810 49.42715, 
-                         -119.56810 49.42843))", "hasCoordinate = true")
-occ_download_import(key = "0026762-190415153152247")
-occ_download_meta(key = "0026762-190415153152247")
+# download prep
+# occ_download_prep("basisOfRecord = HUMAN OBSERVATION", format = "SIMPLE_CSV")
+occ_download_queue(
+  occ_download("geometry = POLYGON ((-119.5681 49.42843, -119.5575 49.43965, -119.54 49.42743, -119.5681 49.42843))"))
+occ_download_meta(key = "0026779-190415153152247")
+occ_download_import(key = "0026778-190415153152247")
+
 
 ## PLOTTING
 library(lawn)
@@ -60,24 +57,8 @@ tweed_wkt <- st_as_text(tweed_simple) # convert to wkt
 ## SAMPLE DOWNLOAD FROM SCKOTT
 occ_download('taxonKey = 7228682', 'hasCoordinate = TRUE', 'hasGeospatialIssue = FALSE', 
              'geometry = POLYGON((30.1 10.1, 10 20, 20 40, 40 40, 30.1 10.1))')
-
-# working download request
-tweed_full <- occ_download('geometry = POLYGON ((-126.61890 52.82582, -126.61470 52.99063, 
-                           -126.79130 53.00253, -126.83990 53.10636, -126.99290 53.15149, 
-                           -127.06030 53.29415, -127.25340 53.26204, -127.30520 53.33956, 
-                           -127.21260 53.39549, -127.04150 53.39694, -126.96750 53.47152, 
-                           -126.77870 53.53582, -126.70590 53.66283, -126.48070 53.80835, 
-                           -126.35340 53.81418, -126.08430 53.75325, -126.08320 53.24568, 
-                           -126.01170 53.20708, -125.77080 53.20707, -125.77370 52.85126, 
-                           -125.64010 52.73904, -125.63980 52.64575, -125.72770 52.56943, 
-                           -125.73020 52.42860, -125.61620 52.31243, -125.64810 52.12862, 
-                           -125.58810 51.89234, -125.91530 51.89921, -125.92420 52.10753, 
-                           -126.06350 52.34376, -126.44870 52.64625, -126.35080 52.70379, 
-                           -126.34260 52.82858, -126.51460 52.77071, -126.61890 52.82582))', 
-                           'hasCoordinate = TRUE')
-tweed_full # view
-
-occ_download_get("0025301-181108115102211", path = "tweed_full.zip", overwrite = T, curlopts = list())
+occ_download_meta(key = "0026785-190415153152247")
+occ_download_get("0026785-190415153152247", path = "tweed_full.zip", overwrite = T, curlopts = list())
 occ_download_dataset_activity("0025301-181108115102211")
 
 ## leaflet
