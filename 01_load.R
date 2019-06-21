feeab624b0355fd8a4449408f1491ea7e641e96e
Julie Galloway <jgalloway@uvic.ca>
2019-06-21 09:19:07 -0700

    update to get_data

--- 01_load.R
+++ 01_load.R
@@ -1,21 +1,24 @@
 ##
 ## BCPARKS/RGBIF SPATIAL DATA ACQUISITON TOOL ----------------------
 ## 
-
 # load packages
-Packages <- c("spocc", "sf", "tidyverse", "rgbif", "viridis", "dplyr", 
+Packages <- c("spocc", "sf", "tidyverse", "rgbif", "viridis", "rinat", 
               "maptools", "devtools", "wellknown","geojsonsf", "leaflet", 
               "jsonlite", "rgbif", "geojsonio", "bcdata", "rmapshaper", 
               "RColorBrewer", "RCurl", "bcmaps", "geojson")
 lapply(Packages, library, character.only = TRUE)
+remotes::install_github("ropensci/wicket@reverse")
 
 ## ENVIRONMENT SETTINGS ----------------------------------------
 ## *Note: Fill in the variables below with your own username/pwd
 ## 
 # user_renviron <- path.expand(file.path("~", ".Renviron"))
-# Sys.setenv(GBIF_USER = "jgalloway93")
-# Sys.setenv(GBIF_PWD = "*********")
-# Sys.setenv(GBIF_EMAIL = "jgalloway@uvic.ca")
+Sys.setenv(GBIF_USER = "*")
+Sys.setenv(GBIF_PWD = "*")
+Sys.setenv(GBIF_EMAIL = "*")
+Sys.setenv(LIBCURL_BUILD="winssl")
+install.packages('curl', type = "source")
+library(curl)
 
 ## LOADING DATA --------------------------------
 ## --
@@ -24,4 +27,4 @@ lapply(Packages, library, character.only = TRUE)
 # remotes::install_github("bcgov/bcdata")
 
 # Download BC parks data as sf
-parks <- bcdc_get_geodata("1130248f-f1a3-4956-8b2e-38d29d3e4af7", crs = 4326)
+parks <- bcdc_get_data("1130248f-f1a3-4956-8b2e-38d29d3e4af7", crs = 4326)
