21dc7471f113bd2d3d928a3b8b3dbe7e1c9e2c64
Julie Galloway <jgalloway@uvic.ca>
2019-08-21 11:24:46 -0700

    line to download from umbrella project - not working

--- 04_rinat_output.R
+++ 04_rinat_output.R
@@ -1,8 +1,8 @@
-##
 ## Using rinat
 ## Retrieve info about a project
-strathcona <- get_inat_obs_project("strathcona-provincial-park", type = "info", 
-                                  raw = F)
+strathcona <- get_inat_obs_project("strathcona-provincial-park", 
+                                   type = "info", raw = T)
+allparks <- get_inat_obs_project("bc-parks", type = "info", raw = T)
 
 ## Now retrieve all the observations for that project
 strathcona <- get_inat_obs_project(strathcona$id, 
